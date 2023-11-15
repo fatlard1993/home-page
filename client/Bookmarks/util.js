561fc6699039c2cb2ec6b4b0317983a747140d5d
justfatlard <fatlard1993@gmail.com>
2023-11-14 23:29:06 -0800

    Fix button links

--- client/Bookmarks/util.js
+++ client/Bookmarks/util.js
@@ -5,7 +5,11 @@ const localhostRegex = /^(.+:\/\/)?localhost(:\d{1,5})?(\/.*)?$/;
 
 export const isLink = url => websiteRegex.test(url) || ipRegex.test(url) || localhostRegex.test(url) || hostnameRegex.test(url);
 
-export const fixLink = url => (isLink(url) && !/.+:\/\//.test(url) ? `http://${url}` : `http://google.com/search?q=${url}`);
+export const fixLink = url => {
+	if (isLink(url)) return url;
+
+	return !/.+:\/\//.test(url) ? `http://${url}` : `http://google.com/search?q=${url}`;
+};
 
 export const copyToClipboard = text => {
 	if (!isSecureContext) return false;
