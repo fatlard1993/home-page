9c4027b3aceaa686e69c9b3a86e134a9750a25b3
justfatlard <fatlard1993@gmail.com>
2025-06-22 19:34:33 -0700

    add close dev command, and build error catch

--- server/index.js
+++ server/index.js
@@ -38,9 +38,16 @@ database.init({ persistent: options.persistent, path: options.database });
 
 server.init({ port: options.port });
 
-for await (const line of console) {
-	if (line === 'b') {
-		console.log('>> Building...');
-		spawnBuild();
+if (process.env.NODE_ENV === 'development') {
+	try {
+		for await (const line of console) {
+			if ({ stop: 1, close: 1, exit: 1 }[line]) process.kill(process.pid, 'SIGTERM');
+			else if (line === 'b') {
+				console.log('>> Building...');
+				spawnBuild();
+			}
+		}
+	} catch (error) {
+		console.error('Build Error', error);
 	}
 }
